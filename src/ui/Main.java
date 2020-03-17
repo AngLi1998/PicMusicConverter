399cd870d9d5616087c2a413a5d1b3192f99328f
Yifei Chen <yifeichen@163.com>
2020-03-16 22:23:45 -0700

    Music added

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,17 +1,27 @@
 package ui;
 
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Random;
+
 
 public class Main extends JFrame {
     private JPanel contentPane;
     private JPanel panel = new JPanel();
     private JLabel picture = new JLabel();
-
+    private Music music;
+    private String font = "Tempus Sans ITC";
+    private int size = 20;
 
     public static void main(String[] args) {
         Main m = new Main();
@@ -21,7 +31,7 @@ public class Main extends JFrame {
 
     public void Player(){
 
-        setTitle("picture Player");
+        setTitle("Picture Player");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         setBounds(100, 100, 852, 603);
@@ -32,7 +42,8 @@ public class Main extends JFrame {
 
         JLabel lblRecommendation = new JLabel("Recommendation");
         lblRecommendation.setHorizontalAlignment(SwingConstants.CENTER);
-        lblRecommendation.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 28));
+
+        lblRecommendation.setFont(new Font(font, Font.PLAIN, 28));
         lblRecommendation.setBounds(463, 58, 270, 53);
         contentPane.add(lblRecommendation);
 
@@ -49,58 +60,150 @@ public class Main extends JFrame {
 
                     ImageIcon img = new ImageIcon(path);
                     picture.setIcon(upload(img));
+                    lblRecommendation.setText("Recommendation:");
                 }else{
                     System.out.println("No File Selected");
                 }
             }
         });
-        btnUpLoad.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 23));
+
+        btnUpLoad.setFont(new Font(font, Font.PLAIN, size));
         btnUpLoad.setBounds(103, 439, 226, 53);
         contentPane.add(btnUpLoad);
 
-        JButton btnTypeOne = new JButton("Type One");
-        btnTypeOne.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 23));
+        JButton btnTypeOne = new JButton("Action");
+        btnTypeOne.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                try{
+                    music.stop();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                String path = getMusicPath("src/music/action");
+
+                music = new Music(path);
+                music.start();
+                lblRecommendation.setText("Action");
+            }
+
+        });
+        btnTypeOne.setFont(new Font(font, Font.PLAIN, size));
         btnTypeOne.setBounds(439, 124, 159, 53);
         contentPane.add(btnTypeOne);
 
-        JButton btnTypeTwo = new JButton("Type One");
+        JButton btnTypeTwo = new JButton("Beauty & Style");
         btnTypeTwo.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
+                try{
+                    music.stop();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                String path = getMusicPath("src/music/beauty & style");
+                music = new Music(path);
+                lblRecommendation.setText("Beauty & Style");
+                music.start();
             }
         });
-        btnTypeTwo.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 23));
+        btnTypeTwo.setFont(new Font(font, Font.PLAIN, size));
         btnTypeTwo.setBounds(623, 124, 159, 53);
         contentPane.add(btnTypeTwo);
 
-        JButton btnTypeThree = new JButton("Type One");
-        btnTypeThree.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 23));
+        JButton btnTypeThree = new JButton("Education");
+        btnTypeThree.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                try{
+                    music.stop();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                String path = getMusicPath("src/music/education");
+                music = new Music(path);
+                lblRecommendation.setText("Education");
+                music.start();
+            }
+        });
+        btnTypeThree.setFont(new Font(font, Font.PLAIN, size));
         btnTypeThree.setBounds(439, 190, 159, 53);
         contentPane.add(btnTypeThree);
 
-        JButton btnTypeFour = new JButton("Type One");
-        btnTypeFour.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 23));
+        JButton btnTypeFour = new JButton("Entertainment");
+        btnTypeFour.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                try{
+                    music.stop();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                String path = getMusicPath("src/music/entertaiment");
+                music = new Music(path);
+                lblRecommendation.setText("Entertainment");
+                music.start();
+            }
+        });
+        btnTypeFour.setFont(new Font(font, Font.PLAIN, size));
         btnTypeFour.setBounds(623, 190, 159, 53);
         contentPane.add(btnTypeFour);
 
-        JButton btnTypeFive = new JButton("Type One");
-        btnTypeFive.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 23));
+        JButton btnTypeFive = new JButton("New & Politics");
+        btnTypeFive.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                try{
+                    music.stop();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                String path = getMusicPath("src/music/news & politics");
+                music = new Music(path);
+                lblRecommendation.setText("New & Politics");
+                music.start();
+            }
+        });
+        btnTypeFive.setFont(new Font(font, Font.PLAIN, size));
         btnTypeFive.setBounds(439, 255, 159, 53);
         contentPane.add(btnTypeFive);
 
-        JButton btnTypeSix = new JButton("Type One");
-        btnTypeSix.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 23));
+        JButton btnTypeSix = new JButton("Tech & Science");
+        btnTypeSix.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                try{
+                    music.stop();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                String path = getMusicPath("src/music/tech & science");
+                music = new Music(path);
+                lblRecommendation.setText("Tech & Science");
+                music.start();
+            }
+        });
+        btnTypeSix.setFont(new Font(font, Font.PLAIN, size));
         btnTypeSix.setBounds(623, 256, 159, 53);
         contentPane.add(btnTypeSix);
 
-        JButton btnTypeSeven = new JButton("Type One");
-        btnTypeSeven.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 23));
+        JButton btnTypeSeven = new JButton("Travel");
+        btnTypeSeven.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                try{
+                    music.stop();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                String path = getMusicPath("src/music/travel");
+                music = new Music(path);
+                lblRecommendation.setText("Travel");
+                music.start();
+            }
+        });
+        btnTypeSeven.setFont(new Font(font, Font.PLAIN, size));
         btnTypeSeven.setBounds(439, 321, 159, 53);
         contentPane.add(btnTypeSeven);
 
-        JButton btnLilke = new JButton("Lilke");
-        btnLilke.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 23));
-        btnLilke.setBounds(556, 439, 226, 53);
-        contentPane.add(btnLilke);
+        JButton btnLike = new JButton("Like");
+        btnLike.setFont(new Font(font, Font.PLAIN, size));
+        btnLike.setBounds(556, 439, 226, 53);
+        contentPane.add(btnLike);
 
 
         panel.setBounds(22, 46, 400, 300);
@@ -110,12 +213,6 @@ public class Main extends JFrame {
         panel.add(picture);
         picture.setHorizontalAlignment(SwingConstants.CENTER);
 
-//        ImageIcon img = new ImageIcon("C:\\Users\\Yifei Chen\\Desktop\\u=2835156641,1453869869&fm=26&gp=0.jpg");
-//        int sH = (int) scaleH(panel.getHeight(), panel.getWidth(), img.getIconHeight(), img.getIconWidth());
-//        int sW = (int) scaleW(panel.getHeight(), panel.getWidth(), img.getIconHeight(), img.getIconWidth());
-//
-//        Image temp = img.getImage().getScaledInstance(sW, sH, img.getImage().SCALE_DEFAULT);
-//        ImageIcon icon = new ImageIcon(temp);
         ImageIcon img = new ImageIcon(getClass().getResource("../PlusSign.jpg"));
         ImageIcon icon = upload(img);
         picture.setIcon(icon);
@@ -141,7 +238,37 @@ public class Main extends JFrame {
         return icon;
     }
 
+    private String getMusicPath(String path) {
+        File dir = new File(path);
+        File[] files = dir.listFiles();
+        Random rand = new Random();
+        return files[rand.nextInt(files.length)].getAbsolutePath();
+    }
+
+    class Music extends Thread{
+        private String path;
+
+        public Music(String path) {
+            this.path = path;
+        }
+
+        public void run(){
+
+            try {
+
+                FileInputStream fileInputStream = new FileInputStream(path);
+                Player player = new Player(fileInputStream);
+                player.play();
+
+            }catch(FileNotFoundException e) {
+                e.printStackTrace();
+            }catch(JavaLayerException e) {
+                e.printStackTrace();
+            }
+
+        }
 
 
+    }
 
 }
